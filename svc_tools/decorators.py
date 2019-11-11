bd7ed21e21cfdfc36c2fb4f4193dc4422d285146
Tyler Wince <tyler@myndshft.com>
2019-11-11 16:42:32 -0700

    added json_timer

--- svc_tools/decorators.py
+++ svc_tools/decorators.py
@@ -45,3 +45,48 @@ def timer(log_level: Callable):
         return new_timed_func
 
     return decorator
+
+
+def json_timer(log_level: Callable):
+    """Time a function and log the time it took to execute.
+
+    If you want to know how long a function takes to execute
+    ang log alongside your application logs, then decorate
+    your function with this.
+
+    :param log_level: logging method to use in call
+    :returns: Whatever your original function returns
+
+    Usage ::
+      >>> from svc_tools import timer
+      >>> from time import sleep
+      >>> import logging
+      >>> @timer(logging.warning)
+      >>> def myFunc(sleep_time):
+      ...     # whatever you want to time
+      ...     sleep(sleep_time)
+      >>> myFunc(10)
+      WARNING:root:myFunc took 10.000502109527588 to run.
+
+    """
+
+    def decorator(func):
+        """Decorate the original function."""
+
+        @wraps(func)
+        def new_timed_func(*args, **kwargs):
+            """Do the timing."""
+            start_time = time.time()
+            returned_value = func(*args, **kwargs)
+            total_time = time.time() - start_time
+            log_level(
+                {
+                    "message": f"{func.__name__} took {total_time} to run.",
+                    "total_time": total_time,
+                }
+            )
+            return returned_value
+
+        return new_timed_func
+
+    return decorator
